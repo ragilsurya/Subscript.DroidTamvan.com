23b34dd6d19e4e257f9f9352ffff1898079046d1
ragilsurya <33425610+ragilsurya@users.noreply.github.com>
2019-03-10 23:27:58 +0700

    Create SAMPM V2.3.js

---
+++ SAMPM V2.3.js
@@ -0,0 +1,19 @@
+var ab = 0;
+function asdff() {
+  window.open(&#39;https://www.youtube.com/DroidTamvan&#39;);
+
+
+ab = ab + 5;
+document.getElementById(&#39;button3&#39;).style.backgroundColor = &#39;#09ab09&#39;;
+document.getElementById(&#39;button3&#39;).value = &quot;*UNLOCKED* Click to GET LINK :D&quot;;}
+function asdf() {
+
+if ( ab == 5){
+window.open(&#39;https://tinyurl.com/y6sskzdw&#39;);
+
+}
+else {}
+}
+function adf(){
+window.open(&#39;you-dont-need-this&#39;)
+}
